0012364b1764256ba6ed3a47f5d4588afeb2092c
Goose <tilek@yaros.kg>
2019-12-23 23:43:06 +0600

    +resurrecting project +add new design +delete old classes and layout's

--- app/src/main/java/com/goose/player/enums/Pages.kt
+++
@@ -1,9 +0,0 @@
-package com.goose.player.enums
-
-/**
- *Created by Gxxxse on 20.07.2019.
- */
-enum class Pages(val number: Int) {
-    PlayerPage(0),
-    SongsListPage(1)
-}
--- app/src/main/java/com/goose/player/enums/PlaybackStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.goose.player.enums
-
-/**
- *Created by Gxxxse on 05.08.2019.
- */
-enum class PlaybackStatus {
-    PLAYING,
-    PAUSED
-}
